a0f7391180523e07cf034556d842ac06972bc88a
Yongseok Kwon <kwonys@umich.edu>
2021-12-17 02:13:41 -0500

    change the default

--- gym/experiment.py
+++ gym/experiment.py
@@ -300,8 +300,8 @@ def experiment(
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='hopper')
-    parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
+    parser.add_argument('--env', type=str, default='fetch')
+    parser.add_argument('--dataset', type=str, default='expert')  # medium, medium-replay, medium-expert, expert
     parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
     parser.add_argument('--K', type=int, default=60)
     parser.add_argument('--pct_traj', type=float, default=1.)
@@ -316,7 +316,7 @@ if __name__ == '__main__':
     parser.add_argument('--weight_decay', '-wd', type=float, default=1e-4)
     parser.add_argument('--warmup_steps', type=int, default=10000)
     parser.add_argument('--num_eval_episodes', type=int, default=100)
-    parser.add_argument('--max_iters', type=int, default=30) ## more than 10?
+    parser.add_argument('--max_iters', type=int, default=35) ## more than 10?
     parser.add_argument('--num_steps_per_iter', type=int, default=10000)
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--log_to_wandb', '-w', type=bool, default=False)
