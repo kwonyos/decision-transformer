420b9a8379654b0f5671f91559ed640218fa54ac
Yongseok Kwon <kwonys@umich.edu>
2021-12-22 02:18:55 -0500

    tune K and increase iterations

--- gym/experiment.py
+++ gym/experiment.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
     parser.add_argument('--env', type=str, default='fetch')
     parser.add_argument('--dataset', type=str, default='expert')  # medium, medium-replay, medium-expert, expert
     parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
-    parser.add_argument('--K', type=int, default=20)
+    parser.add_argument('--K', type=int, default=30)
     parser.add_argument('--pct_traj', type=float, default=1.)
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--model_type', type=str, default='dt')  # dt for decision transformer, bc for behavior cloning
@@ -319,7 +319,7 @@ if __name__ == '__main__':
     parser.add_argument('--weight_decay', '-wd', type=float, default=1e-4)
     parser.add_argument('--warmup_steps', type=int, default=10000)
     parser.add_argument('--num_eval_episodes', type=int, default=100)
-    parser.add_argument('--max_iters', type=int, default=15) ## more than 10?
+    parser.add_argument('--max_iters', type=int, default=60) ## more than 10?
     parser.add_argument('--num_steps_per_iter', type=int, default=10000)
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--log_to_wandb', '-w', type=bool, default=True)
